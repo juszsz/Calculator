da6fbf7afe7a461fec4b686eb39ed34bb2fa606f
juszsz <justinlarrydimaiwat@gmail.com>
2026-09-18 08:43:07 +0800

    added the displays in js

---
+++ index.js
@@ -0,0 +1,12 @@
+// For the display
+const numOne = document.querySelector("#numOne");
+const operatorType = document.querySelector("#operatorType");
+const numTwo = document.querySelector("#numTwo");
+const equalSign = document.querySelector("#equalSign");
+const answer = document.querySelector("#answer");
+
+numOne.textContent = 12;
+operatorType.textContent = "+"
+numTwo.textContent = 12;
+equalSign.textContent = "=";
+answer.textContent = 24;
